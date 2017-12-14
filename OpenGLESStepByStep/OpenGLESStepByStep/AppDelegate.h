a1a26420a311cffa272bbcb422a778287911c6a5
zyqhi <zyqhi2010@gmail.com>
2017-12-14 17:32:49 +0800

    create OpenGLES step by step project

---
+++ OpenGLESStepByStep/OpenGLESStepByStep/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGLESStepByStep
+//
+//  Created by zyq on 14/12/2017.
+//  Copyright © 2017 Mutsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGLESStepByStep/OpenGLESStepByStep/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGLESStepByStep
+//
+//  Created by zyq on 14/12/2017.
+//  Copyright © 2017 Mutsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
