1027ce77f47a8c79306980f12f78801175222951
zyqhi <zyqhi2010@gmail.com>
2017-12-14 19:08:09 +0800

    [MILESTONE] Add vertex and fragment shaders

--- OpenGLESStepByStep/OpenGLESStepByStep/OpenGLESDemoView.m
+++ OpenGLESStepByStep/OpenGLESStepByStep/OpenGLESDemoView.m
@@ -17,6 +17,9 @@
 @property (nonatomic, strong) EAGLContext *context;
 @property (nonatomic, assign) GLuint colorRenderBuffer;
 
+@property (nonatomic, assign) GLuint colorSlot;
+@property (nonatomic, assign) GLuint positionSlot;
+
 @end
 
 @implementation OpenGLESDemoView
@@ -75,6 +78,76 @@
     glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
                               GL_RENDERBUFFER, _colorRenderBuffer);
+}
+
+- (GLuint)compileShader:(NSString*)shaderName withType:(GLenum)shaderType {
+    
+    // 1
+    NSString *shaderPath = [[NSBundle mainBundle] pathForResource:shaderName
+                                                           ofType:@"glsl"];
+    NSError *error;
+    NSString *shaderString = [NSString stringWithContentsOfFile:shaderPath
+                                                       encoding:NSUTF8StringEncoding error:&error];
+    if (!shaderString) {
+        NSLog(@"Error loading shader: %@", error.localizedDescription);
+        exit(1);
+    }
+    
+    // 2
+    GLuint shaderHandle = glCreateShader(shaderType);
+    
+    // 3
+    const char *shaderStringUTF8 = [shaderString UTF8String];
+    int shaderStringLength = (int)[shaderString length];
+    glShaderSource(shaderHandle, 1, &shaderStringUTF8, &shaderStringLength);
+    
+    // 4
+    glCompileShader(shaderHandle);
+    
+    // 5
+    GLint compileSuccess;
+    glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, &compileSuccess);
+    if (compileSuccess == GL_FALSE) {
+        GLchar messages[256];
+        glGetShaderInfoLog(shaderHandle, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"%@", messageString);
+        exit(1);
+    }
+    
+    return shaderHandle;
+}
+
+- (void)compileShaders {
+    // 1
+    GLuint vertexShader = [self compileShader:@"SimpleVertex" withType:GL_VERTEX_SHADER];
+    GLuint fragmentShader = [self compileShader:@"SimpleFragment" withType:GL_FRAGMENT_SHADER];
+    
+    // 2
+    GLuint programHandle = glCreateProgram();
+    glAttachShader(programHandle, vertexShader);
+    glAttachShader(programHandle, fragmentShader);
+    glLinkProgram(programHandle);
+    
+    // 3
+    GLint linkSuccess;
+    glGetProgramiv(programHandle, GL_LINK_STATUS, &linkSuccess);
+    if (linkSuccess == GL_FALSE) {
+        GLchar messages[256];
+        glGetProgramInfoLog(programHandle, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"%@", messageString);
+        exit(1);
+    }
+    
+    // 4
+    glUseProgram(programHandle);
+    
+    // 5
+    _positionSlot = glGetAttribLocation(programHandle, "Position");
+    _colorSlot = glGetAttribLocation(programHandle, "SourceColor");
+    glEnableVertexAttribArray(_positionSlot);
+    glEnableVertexAttribArray(_colorSlot);
 }
 
 #pragma mark -
--- README.md
+++ README.md
@@ -8,3 +8,16 @@ https://www.raywenderlich.com/4404/opengl-es-2-0-for-iphone-tutorial-part-2-text
 ## iOS GLKit
 
 https://developer.apple.com/documentation/glkit
+https://www.raywenderlich.com/5223/beginning-opengl-es-2-0-with-glkit-part-1
+
+https://www.raywenderlich.com/9743/how-to-create-a-simple-2d-iphone-game-with-opengl-es-2-0-and-glkit-part-1
+
+GLKit中的shader呢？
+
+## 算法
+
+三角剖分算法(delaunay)
+http://www.cnblogs.com/zhiyishou/p/4430017.html
+
+OpenGL的几何变换3之内观察全景图
+https://www.cnblogs.com/1024Planet/p/5667031.html
