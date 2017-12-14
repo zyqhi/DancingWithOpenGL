97ff9240600f4498f1a9ac74eaf4a53a7b6390f1
zyqhi <zyqhi2010@gmail.com>
2017-12-14 19:20:13 +0800

    [MILESTONE] Render the vertex data

--- OpenGLESStepByStep/OpenGLESStepByStep/OpenGLESDemoView.m
+++ OpenGLESStepByStep/OpenGLESStepByStep/OpenGLESDemoView.m
@@ -11,6 +11,23 @@
 #include <OpenGLES/ES2/gl.h>
 #include <OpenGLES/ES2/glext.h>
 
+typedef struct {
+    float Position[3];
+    float Color[4];
+} Vertex;
+
+const Vertex Vertices[] = {
+    {{1, -1, 0}, {1, 0, 0, 1}},
+    {{1, 1, 0}, {0, 1, 0, 1}},
+    {{-1, 1, 0}, {0, 0, 1, 1}},
+    {{-1, -1, 0}, {0, 0, 0, 1}}
+};
+
+const GLubyte Indices[] = {
+    0, 1, 2,
+    2, 3, 0
+};
+
 @interface OpenGLESDemoView ()
 
 @property (nonatomic, strong) CAEAGLLayer *eaglLayer;
@@ -33,6 +50,7 @@
         [self setupRenderBuffer];
         [self setupFrameBuffer];
         [self compileShaders];
+        [self setupVertexBufferObjects];
         [self render];
     }
     return self;
@@ -80,6 +98,21 @@
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
                               GL_RENDERBUFFER, _colorRenderBuffer);
 }
+
+
+- (void)setupVertexBufferObjects {
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    
+    GLuint indexBuffer;
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+}
+
+#pragma mark - Shaders
 
 - (GLuint)compileShader:(NSString*)shaderName withType:(GLenum)shaderType {
     
@@ -156,7 +189,19 @@
 - (void)render {
     glClearColor(0, 104.0/255.0, 55.0/255.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
+    
+    // 1
+    glViewport(0, 0, self.frame.size.width, self.frame.size.height);
+    
+    // 2
+    glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+    glVertexAttribPointer(_colorSlot, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*) (sizeof(float) * 3));
+    
+    // 3
+    glDrawElements(GL_TRIANGLES, sizeof(Indices)/sizeof(Indices[0]), GL_UNSIGNED_BYTE, 0);
+    
     [_context presentRenderbuffer:GL_RENDERBUFFER];
 }
 
 @end
+
